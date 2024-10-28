54d0810c319ae8db4ae3cef25be6a8f2e50d0b2e
Vux <tuanvux01@gmail.com>
2024-10-29 00:08:32 +0700

    Implement device attribute scope with 3 type: SERVER_SCOPE, SHARED_SCOPE and CLIENT_SCOPE (#55)

--- viot/app/module/device_data/repository/device_attribute_repository.py
+++ viot/app/module/device_data/repository/device_attribute_repository.py
@@ -1,24 +1,45 @@
 from collections.abc import Sequence
+from typing import NamedTuple
 from uuid import UUID
 
 from sqlalchemy import delete, select
 
 from app.database.repository import AsyncSqlalchemyRepository
 
+from ..constants import DeviceAttributeScope
 from ..model.device_attribute import DeviceAttribute
 
 
+class KeyWithScope(NamedTuple):
+    key: str
+    scope: DeviceAttributeScope
+
+
 class DeviceAttributeRepository(AsyncSqlalchemyRepository):
-    async def find_all_keys_by_device_id(self, device_id: UUID) -> Sequence[str]:
-        stmt = select(DeviceAttribute.key).where(DeviceAttribute.device_id == device_id)
+    async def find_all_key_with_scope_by_device_id(self, device_id: UUID) -> list[KeyWithScope]:
+        stmt = select(DeviceAttribute.key, DeviceAttribute.scope).where(
+            DeviceAttribute.device_id == device_id
+        )
+        results = (await self.session.execute(stmt)).fetchall()
+        return [KeyWithScope(key=key, scope=scope) for key, scope in results]
+
+    async def find_all_key_by_scope_and_device_id(
+        self, device_id: UUID, scope: DeviceAttributeScope
+    ) -> Sequence[str]:
+        stmt = (
+            select(DeviceAttribute.key)
+            .where(DeviceAttribute.device_id == device_id)
+            .where(DeviceAttribute.scope == scope)
+        )
         return (await self.session.execute(stmt)).scalars().all()
 
-    async def find_all_by_device_id_and_keys(
-        self, device_id: UUID, keys: set[str]
+    async def find_all_by_device_id_keys_and_scope(
+        self, device_id: UUID, keys: set[str], scope: DeviceAttributeScope
     ) -> Sequence[DeviceAttribute]:
         stmt = (
             select(DeviceAttribute)
             .where(DeviceAttribute.device_id == device_id)
+            .where(DeviceAttribute.scope == scope)
             .where(DeviceAttribute.key.in_(keys))
         )
         return (await self.session.execute(stmt)).scalars().all()
